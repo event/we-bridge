0595c74f0ddb24e67a3d13ab71aab0438c761628
Leonid Movshovich <event.riga@gmail.com>
2010-10-08 00:08:16 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+#  Copyright 2010 Leonid Movshovich <event.riga@gmail.com>
+
+# This file is part of Webridge.
+# Webridge is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# Webridge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with Webridge.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('Hello shitty world!')
+
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler)],
+                                         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
